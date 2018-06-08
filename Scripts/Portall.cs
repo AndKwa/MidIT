0c0857ddafeb8bcf5a17681ce6b25f38ff2d073e
Andrzej <32818801+AndKwa@users.noreply.github.com>
2018-06-08 23:00:27 +0200

    I have just set up the basics

---
+++ Scripts/Portall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portall : MonoBehaviour {
+public GameObject otherPortal;
+public bool portalOn=false;
+private void OnTriggerEnter(Collider other)
+{
+	if(other.CompareTag("Player") && otherPortal.GetComponent<Portall>().portalOn)
+{
+	Rigidbody otherRigidBody = other.GetComponent<Rigidbody>();
+	Vector3 ExitVelocity = otherPortal.transform.forward * otherRigidBody.velocity.magnitude;
+	otherRigidBody.velocity = ExitVelocity;
+	other.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 1.5f;
+}
+}
+}
---
+++ Scripts/Toolbar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toolbar : MonoBehaviour {
+public GameObject tool1,tool2,tool3,highlight;
+	void Update ()
+	{
+		if(Input.GetKey("1"))
+		{
+			tool1.SetActive(true);
+			tool2.SetActive(false);
+			tool3.SetActive(false);
+			highlight.transform.localPosition = new Vector3(-117.5701f, -6.005005f, 0f);
+		}
+		if(Input.GetKey("2"))
+		{
+			tool1.SetActive(false);
+			tool2.SetActive(true);
+			tool3.SetActive(false);
+			highlight.transform.localPosition= new Vector3(-2.570068f, -6.005005f, 0f);
+		}
+		if(Input.GetKey("3"))
+		{
+			tool1.SetActive(false);
+			tool2.SetActive(false);
+			tool3.SetActive(true);
+			highlight.transform.localPosition = new Vector3(112.4299f, -6.005005f, 0f);
+		}
+	}
+}
